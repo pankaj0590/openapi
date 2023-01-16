5f934675440c7c38db2f84ed282d5c6d46a93973
IN002ZLX <IN002ZLX@yellowdc.com>
2023-01-16 21:43:10 +0530

    added spring boot openapi integration

---
+++ spring-boot-openapi/src/test/java/com/pankaj/springbootopenapi/SpringBootOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.springbootopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
